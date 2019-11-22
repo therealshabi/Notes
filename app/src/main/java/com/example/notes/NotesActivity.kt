3727b5e7b2597a66882acb64b4cb35901d9bbdf2
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-23 01:34:53 +0530

    Use staggered grid layout for notes recycler view

--- app/src/main/java/com/example/notes/NotesActivity.kt
+++ app/src/main/java/com/example/notes/NotesActivity.kt
@@ -1,11 +1,15 @@
 package com.example.notes
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
 import com.example.notes.adapter.NotesRecyclerAdapter
 import kotlinx.android.synthetic.main.activity_notes.*
+import timber.log.Timber
 
 class NotesActivity : AppCompatActivity() {
 
@@ -20,12 +24,32 @@ class NotesActivity : AppCompatActivity() {
 
         notesViewModel = ViewModelProviders.of(this).get(NotesViewModel::class.java)
         setupNotesRecyclerView()
+
+        notesViewModel.noteList.observe(this, Observer {
+            notesAdapter.addNotes(it)
+        })
+
+        addNoteFab.setOnClickListener {
+            NoteDialog().show(supportFragmentManager, "Note Dialog")
+        }
+
+        searchEditText.setOnEditorActionListener { searchTextView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val query = searchTextView.text.toString()
+                if (query.isEmpty()) {
+                    return@setOnEditorActionListener true
+                }
+                notesViewModel.getNotes(query)
+                return@setOnEditorActionListener true
+            }
+            false
+        }
     }
 
     private fun setupNotesRecyclerView() {
         notesAdapter = NotesRecyclerAdapter()
         notesRecyclerView.let {
-            it.layoutManager = GridLayoutManager(this, 2)
+            it.layoutManager = StaggeredGridLayoutManager(2, VERTICAL)
             it.adapter = notesAdapter
         }
     }
