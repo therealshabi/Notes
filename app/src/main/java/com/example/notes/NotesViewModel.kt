6cab0f57d4cdf0c2808f69449914e261ff23ae8f
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-22 22:26:32 +0530

    Add methods to NotesViewModel

--- app/src/main/java/com/example/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/notes/NotesViewModel.kt
@@ -2,10 +2,30 @@ package com.example.notes
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.notes.data.NotesDao
+import com.example.notes.data.NotesDatabase
+import com.example.notes.model.Note
+import java.util.*
 
 class NotesViewModel(application: Application) : AndroidViewModel(application) {
+    private val notesDao: NotesDao?
+    var noteList: MutableLiveData<List<Note>>?
 
     init {
+        val database = NotesDatabase.getDatabase(application)
+        notesDao = database?.getNotesDao()
+        noteList = notesDao?.getAllNotes()
+    }
 
+    fun addNote(title: String, content: String) {
+        val note = Note(title = title, content = content, timeStamp = Date().time)
+        notesDao?.addNote(note)
+        noteList = notesDao?.getAllNotes()
+    }
+
+    fun searchNote(query: String) {
+        notesDao?.getNotes(query)
+        noteList = notesDao?.getAllNotes()
     }
 }
