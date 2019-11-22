3879f161f19da6682ae750360cbea71ff110ed9f
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-22 23:18:19 +0530

    Setup NotesRecyclerView in NotesActivity

--- app/src/main/java/com/example/notes/NotesActivity.kt
+++ app/src/main/java/com/example/notes/NotesActivity.kt
@@ -3,16 +3,28 @@ package com.example.notes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.notes.adapter.NotesRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_notes.*
 
 class NotesActivity : AppCompatActivity() {
 
     private lateinit var notesViewModel: NotesViewModel
+    private lateinit var notesAdapter: NotesRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
 
         notesViewModel = ViewModelProviders.of(this).get(NotesViewModel::class.java)
+        setupNotesRecyclerView()
+    }
 
+    private fun setupNotesRecyclerView() {
+        notesAdapter = NotesRecyclerAdapter()
+        notesRecyclerView.let {
+            it.layoutManager = GridLayoutManager(this, 2)
+            it.adapter = notesAdapter
+        }
     }
 }
