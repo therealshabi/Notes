409901fa05fc834efb5c0153f803cbbbe4743c99
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-22 22:04:04 +0530

    Create Notes data Model

---
+++ app/src/main/java/com/example/notes/model/Notes.kt
@@ -0,0 +1,13 @@
+package com.example.notes.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class Notes(
+    @PrimaryKey(autoGenerate = true)
+    val id: String,
+    val title: String,
+    val content: String,
+    val timeStamp: Long
+)
