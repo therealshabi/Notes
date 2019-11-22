f2a3f3ff748f84c834614f372c79173fc3722c5b
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-22 22:26:17 +0530

    Make id of Note Model to an Int type

--- app/src/main/java/com/example/notes/model/Note.kt
+++ app/src/main/java/com/example/notes/model/Note.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "notes")
 data class Note(
     @PrimaryKey(autoGenerate = true)
-    val id: String,
+    val id: Int = 0,
     val title: String,
     val content: String,
     val timeStamp: Long
