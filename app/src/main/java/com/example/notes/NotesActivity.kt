98da71b2897ebbb5581c541b642b9a77bc595be3
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-22 22:03:38 +0530

    Create NotesViewModel and initialize in NotesActivity

--- app/src/main/java/com/example/notes/NotesActivity.kt
+++ app/src/main/java/com/example/notes/NotesActivity.kt
@@ -2,11 +2,17 @@ package com.example.notes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 
 class NotesActivity : AppCompatActivity() {
+
+    private lateinit var notesViewModel: NotesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
+
+        notesViewModel = ViewModelProviders.of(this).get(NotesViewModel::class.java)
+
     }
 }
---
+++ app/src/main/java/com/example/notes/NotesViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.notes
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class NotesViewModel(application: Application) : AndroidViewModel(application) {
+
+    init {
+
+    }
+}
