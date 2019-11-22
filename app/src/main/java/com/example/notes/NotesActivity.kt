93064b50b17dadce3f10a396af0b7c5b888736c6
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-23 02:06:58 +0530

    Add text watcher with timer for search

--- app/src/main/java/com/example/notes/NotesActivity.kt
+++ app/src/main/java/com/example/notes/NotesActivity.kt
@@ -3,6 +3,9 @@ package com.example.notes
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -10,11 +13,15 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
 import com.example.notes.adapter.NotesRecyclerAdapter
 import kotlinx.android.synthetic.main.activity_notes.*
 import timber.log.Timber
+import java.util.*
+
 
 class NotesActivity : AppCompatActivity() {
 
     private lateinit var notesViewModel: NotesViewModel
     private lateinit var notesAdapter: NotesRecyclerAdapter
+
+    private var timer: Timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +40,38 @@ class NotesActivity : AppCompatActivity() {
             NoteDialog().show(supportFragmentManager, "Note Dialog")
         }
 
-        searchEditText.setOnEditorActionListener { searchTextView, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = searchTextView.text.toString()
-                if (query.isEmpty()) {
+        addSearchActions()
+    }
+
+    private fun addSearchActions() {
+        searchEditText.apply {
+            setOnEditorActionListener { searchTextView, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    searchEditText.text.clear()
+                    val query = searchTextView.text.toString()
+                    if (query.isEmpty()) {
+                        return@setOnEditorActionListener true
+                    }
+                    notesViewModel.getNotes(query)
                     return@setOnEditorActionListener true
                 }
-                notesViewModel.getNotes(query)
-                return@setOnEditorActionListener true
+                false
             }
-            false
+
+            addTextChangedListener {
+                doOnTextChanged { _, _, _, _ ->
+                    timer.cancel()
+                }
+                doAfterTextChanged {
+                    timer = Timer()
+                    timer.schedule(object : TimerTask() {
+                        override fun run() {
+                            val query = searchEditText.text.toString()
+                            notesViewModel.getNotes(query)
+                        }
+                    }, 600)
+                }
+            }
         }
     }
 
